ed47c35e58ea58c34fb33589bd486a8136b7bdb8
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-01-19 03:25:54 +0200

    fix pint

--- print_top_stack.c
+++ print_top_stack.c
@@ -9,7 +9,7 @@ void print_top_stack(stack_t **stack, unsigned int line_number)
 {
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, value out of range\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 		printf("%d\n", (*stack)->n);
